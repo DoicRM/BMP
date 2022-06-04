9cd763413fef41e8a0183190abac818120649da1
Doic <michalakradek2@gmail.com>
2022-06-04 20:05:38 +0200

    implemented bitmap reading in main function of program

--- BMP/main.cpp
+++ BMP/main.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
-#include "bitmapMetadata.hpp"
+#include "bitmapReader.hpp"
 
-int main(int arc, char* argv[])
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!";
-
-    exit(0);
+    if (argc != 2)
+    {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+        std::cout << "Error occurred opening file." << std::endl;
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+        return -1;
+    }
+    else {
+        return BitmapReader::readBitmapFile(argv[1]);
+    }
 }
